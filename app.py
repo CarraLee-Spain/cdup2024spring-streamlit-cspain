c5e8076e261fd847795d09f1fdff519833afc7f9
CarraLee Spain <carralee.spain@arcadis.com>
2024-08-01 09:26:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ home_page = st.Page("baseline/homepage.py", title="Home", icon=":material/home:"
 page_list.append(home_page)
 
 
-Alwin_page = st.Page("baseline/carralee_spain_page.py", title="Carra Lee's page", icon=":material/videogame_asset:")
-page_list.append(Alwin_page)
+CarraLee_page = st.Page("baseline/carralee_spain_page.py", title="Carra Lee's page", icon=":material/videogame_asset:")
+page_list.append(CarraLee_page)
 
 
 Challenge_page = st.Page("CHALLENGE_FOLDER/challenge_page.py", title="Challenge page", icon=":material/insert_drive_file:")
